d7ee14f3ced4721566a19fa7a0877540aadc76ca
Rohnak Agarwal <rrka79wal@gmail.com>
2021-12-21 22:39:11 +0530

    code improved

--- citation_count.py
+++ citation_count.py
@@ -8,29 +8,26 @@ data_path = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1])
 dataset = pd.read_csv(os.path.join(data_path, "content", "citations.csv"))
 X = dataset.iloc[:, 2:8].values  # 8 lekhile 7 jaye naba
 y = dataset.iloc[:, 8].values
-print("--------------------X----------------------------")
+print("--------------------X1----------------------------")
 print(X)
-print("--------------------Y----------------------------")
+print("--------------------Y1----------------------------")
 print(y)
 
 
 le = LabelEncoder()
 X[:, 1] = le.fit_transform(X[:, 1])
-print("--------------------X----------------------------")
+print("--------------------X2----------------------------")
 print(X)
 
 ct = ColumnTransformer(
     transformers=[("encoder", OneHotEncoder(), [1])], remainder="passthrough"
 )
-X = np.array(ct.fit_transform(X))
-print("--------------------X----------------------------")
+Y_fit = np.array(ct.fit_transform(X).toarray())
+print("--------------------X3----------------------------")
 print(X)
 
-X = X[:, 1]
-print("--------------------X----------------------------")
-print(X)
 
 sc = StandardScaler()
-X = sc.fit_transform(X)
-print("--------------------X----------------------------")
-print(X)
+Z = np.array(sc.fit_transform(Y_fit))
+print("--------------------X5----------------------------")
+print(Z)
